817fe8b4a940bdff446d17e60c27fa249cb2b226
KJC <kjc.git@gmail.com>
2013-10-05 11:13:58 -0400

    Initial Version
    Just Some Scripts

---
+++ amazon.scr.scala
@@ -0,0 +1,6 @@
+val getPg = io.Source.fromURL( _ : String ).getLines
+val urls = (1 to 5).map ( x => """http://www.amazon.com/best-sellers-books-Amazon/zgbs/books/ref=zg_bs_books_pg_""" + x + """?_encoding=UTF8&pg=""" + x ).toList
+val htmls = urls.map(getPg)
+val books = htmls.flatMap( _.filter( x=>x.startsWith("http")))
+val bs = books.distinct
+
---
+++ reviewPg.scr.scala
@@ -0,0 +1,52 @@
+
+val url = """http://www.amazon.com/Killing-Jesus-Bill-OReilly/product-reviews/0805098542/ref=dp_top_cm_cr_acr_txt?showViewpoints=1"""
+
+def getReviews( stream : Stream[String] )( implicit acc : List[(String,String)] = List() )
+	: List[(String,String)] = {
+	if(stream.isEmpty) acc else {
+		val nStream = stream.dropWhile(!_.contains("name"))
+		if(nStream.isEmpty) acc else {
+			val nm = nStream.head.dropWhile(_!='=').drop(2).takeWhile(_!='"')
+			val sStream = nStream.dropWhile(!_.contains("out of 5 stars"))
+			if(sStream.isEmpty) acc else {
+				val scrTxt = sStream.head
+				val idx = scrTxt.indexOf("title") + 7
+				val scr = scrTxt.substring( idx, idx +3 )
+
+				val pStream = sStream.dropWhile(!_.contains("profile"))
+				if (pStream.isEmpty) acc else {
+					val profileTxt = pStream.head
+					val idx = profileTxt.indexOf("profile") + 8
+					val profile = profileTxt.drop(idx).takeWhile(_!='>')
+					val newStream = pStream.dropWhile(!_.contains("comment"))
+					getReviews(newStream)( (profile,scr) :: acc )
+				}
+			}
+		}
+	}
+}
+
+
+def getNextReviewURL( currentPage : Stream[String] ) : Option[String] = {
+	val htmlLine = currentPage.filter( x=> x.contains("Previous") && x.contains("Next") && x.contains("href") ).distinct
+	if(htmlLine.size !=1 ) None else { 
+		val hrefs = htmlLine.head.split("href")
+		val href = hrefs.filter(_.contains(">Next"))
+		if(href.size !=1 ) None else {
+			val hrefTxt = href.head
+			Some(hrefTxt.dropWhile(_!='"').drop(1).takeWhile(_!='"'))
+		}
+	}
+}
+
+def getAllReviews( stream : Stream[String] )(implicit acc : List[(String,String)] = List() ) : List[(String,String)] = {
+	val stream2 = stream.dropWhile(!_.contains("""table id="productReviews""""))
+	val rev1 = getReviews( stream2 )( acc )
+	getNextReviewURL( stream2 ) match {
+		case None => acc
+		case Some(nextURL) => getAllReviews( getPg(nextURL).toStream )(rev1)
+	}
+}
+
+
+			
